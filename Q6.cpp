db7b25d08df99d8505d0d2029d0394e5f7201778
Sam <huy.le09@protonmail.com>
2022-03-08 22:08:21 -0800

    Working

--- Q6.cpp
+++ Q6.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <limits.h>
 #include <algorithm>
+#include <string>
+#include <vector>
 
 #include "utils.h"
 #include "Q6.h"
@@ -30,26 +32,65 @@ void Q6()
     }
 
     double smallest_normalized_distance = INT_MAX;
-    int chosen_key = -1;
+    int chosen_keysize = -1;
     for (int KEYSIZE = 2; KEYSIZE <= 40; ++KEYSIZE)
     {
-        int blocks = input.size() / KEYSIZE;
+        int pairs = (input.size() / KEYSIZE) - 1;
         double edit_distance = 0;
-        for (int block_no = 0; block_no < blocks - 1; ++block_no)
+        for (int pair_no = 0; pair_no < pairs; ++pair_no)
         {
-            int first = block_no * KEYSIZE, second = first + KEYSIZE;
+            int first = pair_no * KEYSIZE, second = first + KEYSIZE;
             for (int offset = 0; offset < KEYSIZE; ++offset)
             {
                 edit_distance += count_set_bit(input[first + offset] ^ input[second + offset]);
             }
         }
-        double average = (double)(edit_distance / blocks);
+        double average = (double)(edit_distance / pairs);
         double normalized = (double)(average / (KEYSIZE * 32));
         if (normalized < smallest_normalized_distance)
         {
             smallest_normalized_distance = normalized;
-            chosen_key = KEYSIZE;
+            chosen_keysize = KEYSIZE;
         }
     }
-    std::cout << chosen_key << "\n";
+
+    printf("CHOSEN KEYSIZE: %d\n", chosen_keysize);
+
+    std::vector<std::string> blocks(chosen_keysize, "");
+    std::string repeating_key(chosen_keysize, '\0');
+
+    // Separate into blocks
+    for (int i = 0; i < input.size(); ++i)
+    {
+        int block_no = i % blocks.size();
+        blocks[block_no] += input[i];
+    }
+
+    for (int block_no = 0; block_no < blocks.size(); ++block_no)
+    {
+        int rank = 0;
+        unsigned char block_key = 0;
+        for (int i = 0; i < 128; ++i)
+        {
+            std::string res = xor_ascii_against_mono_char_string(blocks[block_no], i);
+            int count = 0;
+            for (auto ch : res)
+            {
+                if (isalpha(ch) || ch == 32)
+                {
+                    ++count;
+                }
+            }
+            if (count > rank)
+            {
+                rank = count;
+                block_key = i;
+            }
+        }
+        repeating_key[block_no] = block_key;
+    }
+
+    printf("REPEATING KEY: %s\n", repeating_key.c_str());
+
+    std::cout << xor_ascii_against_repeating_key(input, repeating_key);
 }
