55f6d94933d0a6d38dceac2041eb8ab617529d47
Sam <huy.le09@protonmail.com>
2022-03-14 09:06:22 -0700

    Fix print statement

--- Q7.cpp
+++ Q7.cpp
@@ -9,7 +9,7 @@ void Q7()
     std::string input = read_file_base64("./q7_input.txt");
     AES128 *cryptool = new AES128("YELLOW SUBMARINE");
     cryptool->set_ciper_text(input);
-    cryptool->decrypt();
-    cryptool->show_decrypted_message();
+    cryptool->decrypt("ecb");
+    std::cout << cryptool->show_decrypted_message() << "\n";
     delete (cryptool);
 }
--- Q7.h
+++ Q7.h
@@ -1,1 +1,6 @@
+#ifndef _Q7_
+#define _Q7_
+
 void Q7();
+
+#endif
