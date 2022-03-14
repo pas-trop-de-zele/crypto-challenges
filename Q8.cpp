118c2206f8612ca9ac4acc7a26e6f2ed4172a2d5
Sam <huy.le09@protonmail.com>
2022-03-14 09:03:54 -0700

    Refactor Q8 with is_ECB_encrypted() from utils

--- Q8.cpp
+++ Q8.cpp
@@ -4,29 +4,21 @@
 #include <unordered_set>
 
 #include "Q8.h"
+#include "AES128.h"
 
 void Q8()
 {
     std::ifstream file("./q8_input.txt");
     std::string line;
 
-    std::unordered_set<std::string> blocks;
-
     int line_no = 0;
     while (std::getline(file, line))
     {
-        // Input is in hex form we take 32 character per block (2 hex per ascii)
-        for (int i = 0; i < line.size(); i += 32)
+        if (AES128::is_ECB_encrypted(line))
         {
-            std::string block = std::string(line.begin() + i, line.begin() + i + 32);
-            if (blocks.find(block) != blocks.end())
-            {
-                std::cout << "LINE NUMBER: " << line_no << " - " << line << "\n";
-                exit(0);
-            }
-            blocks.insert(std::move(block));
+            std::cout << "LINE NUMBER " << line_no << " - " << line << "\n";
+            return;
         }
-        blocks.clear();
         ++line_no;
     }
 }
--- Q8.h
+++ Q8.h
@@ -1,1 +1,6 @@
+#ifndef _Q8_
+#define _Q8_
+
 void Q8();
+
+#endif
