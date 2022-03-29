fca5e5c8f1b71fde8ef8aa2329d2788fdaddbc72
Sam <huy.le09@protonmail.com>
2022-03-28 18:58:36 -0700

    Finish Q12

--- Q12.cpp
+++ Q12.cpp
@@ -1,20 +1,54 @@
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <memory>
+
 #include "aes128-ecb-cbc/AES128.h"
 #include "Q12.h"
 #include "utils.h"
-#include <string>
 
-std::string Q12_encrypt_aes128_ecb(std::string known, std::string unknown, std::string key)
+std::string KEY = "YELLOW SUBMARINE";
+std::string CUSTOM_INPUT(15, 'A');
+std::unique_ptr<AES128> crypto = std::make_unique<AES128>(KEY);
+
+std::string Q12_encrypt_aes128_ecb(const std::string &known, const std::string &unknown)
 {
-    AES128 crypto(key);
     std::string combined = known + unknown;
-    crypto.set_secret_message(combined);
-    crypto.encrypt_ecb();
-    return crypto.show_encrypted_message();
+    crypto->set_secret_message(combined);
+    crypto->encrypt("ecb");
+    return crypto->show_encrypted_message();
 }
 
 void Q12()
 {
-    std::string KEY = "YELLOW SUBMARINE";
-    std::string input = read_file_base64("./Q12_input.txt");
-    std::cout << Q12_encrypt_aes128_ecb("AAAAAAAAAAAAAAAA", input, KEY);
+    std::string unknown_input = read_file_base64("./Q12_input.txt");
+    // std::string unknown_input = "THIS IS THE INPUT";
+
+    std::unordered_map<std::string, unsigned char> lookup;
+
+    for (int i = 0; i < 256; ++i)
+    {
+        std::string unknown;
+        unknown += i;
+
+        // Encrypted message would be show in hex format
+        // Last 2 chars would be the hex of the hex representation of last char
+        std::string encrypted = Q12_encrypt_aes128_ecb(CUSTOM_INPUT, unknown);
+        std::stringstream buffer(encrypted.substr(30, 2));
+        unsigned char last_two_char;
+        buffer >> std::hex >> last_two_char;
+        lookup[encrypted] = (unsigned char)i;
+    }
+
+    std::string input;
+    for (unsigned char ch : unknown_input)
+    {
+        std::string unknown;
+        unknown += (unsigned char)ch;
+
+        std::string encrypted = Q12_encrypt_aes128_ecb(CUSTOM_INPUT, unknown);
+        input += (unsigned char)lookup.at(encrypted);
+    }
+
+    std::cout << input << "\n";
 }
