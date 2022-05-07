98d1245faeca3aef6b652ce99983cceeb583780e
Sam <huy.le09@protonmail.com>
2022-05-06 20:32:33 -0700

    Finish Q13

--- Q13.cpp
+++ Q13.cpp
@@ -1,5 +1,45 @@
+#include <iostream>
+
+#include "aes128-ecb-cbc/AES128.h"
 #include "Q13.h"
+#include "utils.h"
+
+std::string profile_for(const std::string &email)
+{
+    std::string profile;
+    profile += "email=" + email + ".com";
+    profile += "&uid=10";
+    profile += "&role=user";
+    return profile;
+}
 
 void Q13()
 {
+    std::string KEY = "YELLOW SUBMARINE";
+    std::unique_ptr<AES128> crypto = std::make_unique<AES128>(KEY);
+
+    std::string email(9, 'A');
+    std::string encoded_profile = profile_for(email);
+
+    std::cout << "INITIAL PROFILE: " << encoded_profile << "\n";
+
+    crypto->set_secret_message(encoded_profile);
+    crypto->encrypt("ecb");
+
+    std::string first_two_block = (crypto->show_encrypted_message()).substr(0, 64);
+
+    std::string admin_pad = "AAAAAAAAAAadmin";
+    admin_pad += std::string(11, 0xb);
+
+    std::string encoded_admin_pad = profile_for(admin_pad);
+    crypto->set_secret_message(encoded_admin_pad);
+    crypto->encrypt("ecb");
+
+    std::string admin_block = (crypto->show_encrypted_message()).substr(32, 32);
+
+    std::string combined = hex_string_to_ascii(first_two_block) + hex_string_to_ascii(admin_block);
+    crypto->set_ciper_text(combined);
+    crypto->decrypt("ecb");
+
+    std::cout << "ELEVATED PROFILE: " << crypto->show_decrypted_message() << "\n";
 }
--- Q13.h
+++ Q13.h
@@ -1,6 +1,9 @@
 #ifndef _Q13_
 #define _Q13_
 
+#include <string>
+
+std::string profile_for(const std::string &email);
 void Q13();
 
 #endif
