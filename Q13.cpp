85356a6f761e0ae3d758c106eded3c89e8be5c16
Sam <huy.le09@protonmail.com>
2022-05-06 20:36:41 -0700

    Add explanation

--- Q13.cpp
+++ Q13.cpp
@@ -21,22 +21,27 @@ void Q13()
     std::string email(9, 'A');
     std::string encoded_profile = profile_for(email);
 
+    // email=AAAAAAAAA. | com&uid=10&role= | user
     std::cout << "INITIAL PROFILE: " << encoded_profile << "\n";
 
     crypto->set_secret_message(encoded_profile);
     crypto->encrypt("ecb");
 
+    // first two block cut off without role user: "email=AAAAAAAAA. | com&uid=10&role="
     std::string first_two_block = (crypto->show_encrypted_message()).substr(0, 64);
 
     std::string admin_pad = "AAAAAAAAAAadmin";
+    // pad 11 0xb to have admin on a separate block
     admin_pad += std::string(11, 0xb);
 
     std::string encoded_admin_pad = profile_for(admin_pad);
     crypto->set_secret_message(encoded_admin_pad);
     crypto->encrypt("ecb");
 
+    // isolate admin block "admin"
     std::string admin_block = (crypto->show_encrypted_message()).substr(32, 32);
 
+    // combined to get email=AAAAAAAAA. | com&uid=10&role= | admin
     std::string combined = hex_string_to_ascii(first_two_block) + hex_string_to_ascii(admin_block);
     crypto->set_ciper_text(combined);
     crypto->decrypt("ecb");
