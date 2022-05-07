aff930849e38db36e99d922a1cfb4985fc39101d
Sam <huy.le09@protonmail.com>
2022-05-07 08:40:27 -0700

    Added get_prefix function

--- Q14.cpp
+++ Q14.cpp
@@ -1,3 +1,25 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
 #include "Q14.h"
 
-void Q14();
+const int MAX_PREFIX_LENGTH = 64;
+
+std::string valid_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+std::string get_prefix()
+{
+    std::string prefix;
+    int count = rand() % MAX_PREFIX_LENGTH + 1;
+    std::cout << count << "\n";
+    for (; count > 0; --count)
+    {
+        prefix += valid_chars[rand() % valid_chars.size()];
+    }
+    return prefix;
+}
+
+void Q14()
+{
+}
