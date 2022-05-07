07741ffe2ba150e38cf7a99aed0245242a2b8ee5
Sam <huy.le09@protonmail.com>
2022-05-07 13:52:38 -0700

    Finish Q15

--- Q15.cpp
+++ Q15.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <vector>
 
 #include "Q15.h"
 
@@ -18,27 +19,59 @@ bool is_pkcs7_padded(const std::string &s)
     if (s.size() % BLOCK_SIZE)
         return false;
 
-    unsigned char pad = s[s.size() - 1];
-    for (int i = s.size() - pad; i < s.size(); ++i)
+    std::string last_block = s.substr(s.size() - BLOCK_SIZE, BLOCK_SIZE);
+    unsigned char pad = last_block[last_block.size() - 1];
+    for (int i = last_block.size() - pad; i < BLOCK_SIZE; ++i)
     {
-        if (s[i] != pad)
+        if (last_block[i] != pad)
             return false;
     }
     return true;
 }
 
+void display_test_case(const std::string &s)
+{
+    for (unsigned char c : s)
+    {
+        if (c <= 16)
+        {
+            std::cout << "\\x0" << c + 0;
+        }
+        else
+        {
+            std::cout << c;
+        }
+    }
+    std::cout << "\n";
+}
+
 void Q15()
 {
-    std::string test = "ICE ICE BABY\x04\x04\x04\x04";
-    std::cout << "Checking " << test << "\n";
-    if (is_pkcs7_padded(test))
+    std::vector<std::string> tests =
+        {
+            "ICE ICE BABY\x04\x04\x04\x04",
+            "0123456789123456ICE ICE BABY\x04\x04\x04\x04",
+            "0123456789123456ICE ICE \x08\x08\x08\x08\x08\x08\x08\x08",
+            "ICE ICE\x09\x09\x09\x09\x09\x09\x09\x09\x09",
+            "ICE\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d\x0d",
+            "I\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f",
+            "ICE\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f\x0f",
+            "ICE ICE BABY\x05\x05\x05\x05",
+        };
+
+    for (const std::string &test : tests)
     {
-        strip_padding(test);
-        std::cout << "Pad stripped: " << test << "\n";
-    }
-    else
-    {
-        std::cout << "NOT PADDED CORRECTLY"
-                  << "\n";
+        display_test_case(test);
+        if (is_pkcs7_padded(test))
+        {
+            strip_padding(test);
+            std::cout << "Pad stripped: " << test << "\n";
+        }
+        else
+        {
+            std::cout << "NOT PADDED CORRECTLY"
+                      << "\n";
+        }
+        std::cout << "\n";
     }
 }
--- Q15.h
+++ Q15.h
@@ -1,6 +1,12 @@
 #ifndef _Q15_
 #define _Q15_
 
+#include <string>
+
+void strip_padding(std::string s);
+bool is_pkcs7_padded(const std::string &s);
+void display_test_case(const std::string &s);
+
 void Q15();
 
 #endif
